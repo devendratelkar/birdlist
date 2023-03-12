43babc05c6706a999b07b28f1c50278097a569ef
Akash Telkar <akash_telkar@persistent.com>
2023-03-13 01:19:19 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bird List
+        </p>
+        <a>
+          This is the collection of all birds
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
